f2b17b594502adeba6925b8c7f80c55c30256674
unknown <brendanawjang@gmail.com>
2022-03-26 17:47:03 +0800

    Clean redundant code

--- Models.r
+++ Models.r
@@ -1,54 +1,5 @@
 setwd(getwd())
 
-#### GENERATING SYNTHETIC BALANCED DATA WITH ROSE ALGORITHM ####
-# library(data.table)
-# clean = read.csv("clean.csv", stringsAsFactors=T)
-# clean$msno = factor(clean$msno)
-# clean$gender = factor(clean$gender, levels=c("male","female"), labels =c("male","female"))
-# clean$transaction_date = NULL
-# clean$membership_expire_date = NULL
-# clean$registration_init_time = NULL
-# clean$date = NULL
-# clean$corrected_churn = NULL
-# clean$is_churn = factor(clean$is_churn, levels= c(0,1), labels=c("not churn","churn"))
-# clean$is_auto_renew = factor(clean$is_auto_renew, levels= c(0,1), labels=c("non-auto renewal","auto-renewal"))
-# clean$is_cancel = factor(clean$is_cancel, levels= c(0,1), labels=c("non-cancellation","cancellation"))
-# clean$city = factor(clean$city)
-# clean$registered_via = factor(clean$registered_via)
-# clean$payment_plan_days = factor(clean$payment_plan_days)
-# clean$payment_method_id = factor(clean$payment_method_id)
-# clean = na.omit(clean)
-
-# 
-# library(ROSE)
-# rose = ROSE(is_churn~., data=clean, seed=2407)$data
-# summary(rose$is_churn)
-# write.csv(rose, "rose.csv", row.names = FALSE)
-#### END ROSE ####
-
-### TRY SMOTE ###
-
-# library(smotefamily)
-# library(performanceEstimation)
-# cleandf = read.csv("clean.csv", stringsAsFactors=T)
-# cleandf$transaction_date = NULL
-# cleandf$membership_expire_date = NULL
-# cleandf$registration_init_time = NULL
-# cleandf$date = NULL
-# cleandf$corrected_churn = NULL
-# cleandf$msno = NULL
-# cleandf$gender = factor(cleandf$gender, levels=c("male", "female"), labels=c(0,1))
-# cleandf$gender = as.numeric(cleandf$gender)
-# cleandf = na.omit(cleandf)
-# 
-# smote = SMOTE(cleandf[,-18], cleandf$is_churn)$data
-# smote2 = smote(is_churn ~ ., cleandf, perc.over=1)
-# 
-# write.csv(smote, "smote.csv", row.names = FALSE)
-# write.csv(smote2, "smote2.csv", row.names = FALSE)
-
-#### END SMOTE ####
-
 #### DATA CLEANING ####
 library(data.table)
 cleandf = read.csv("clean.csv", stringsAsFactors=T)
@@ -65,6 +16,7 @@ cleandf$city = factor(cleandf$city)
 cleandf$registered_via = factor(cleandf$registered_via)
 cleandf$payment_plan_days = factor(cleandf$payment_plan_days)
 cleandf$payment_method_id = factor(cleandf$payment_method_id)
+cleandf$gender = factor(cleandf$gender, levels=c("male", "female"), labels=c("male","female"))
 cleandf = na.omit(cleandf)
 summary(cleandf)
 sum(is.na(cleandf))
@@ -76,8 +28,8 @@ library(InformationValue)
 options(scipen = 999, digits=5)
 #obtain the IV values
 cleandf$is_churn = as.numeric(cleandf$is_churn)-1
-create_infotables(cleandf,y="is_churn")
-
+ivs = create_infotables(cleandf,y="is_churn")
+ivs
 cleandf$is_churn = factor(cleandf$is_churn, levels=c(0,1), labels=c("not churn", "churn"))
 
 #choose only higher IV variables
@@ -99,26 +51,10 @@ cor = findCorrelation(cor, cutoff=0.7)
 #remove correlated columns
 cor
 colnames(df)[cor]
-#we want to keep  paymentplandays based on domain knowledge
-finaldf = df[,-c(cor)]
+finaldf = df[,-c(42, 35, 64, 34, 15, 39, 71, 94, 38)]
+finaldf[, -c(63,57)] = lapply(finaldf[,-c(63,57)], function (x) factor(x))
 
-finaldf[, -62] = lapply(finaldf[,-62], function (x) factor(x))
-finaldf$gender. = NULL
-
-#### OR LOAD SMOTE ####
-# df = read.csv("smote.csv", stringsAsFactors = T)
-# df$gender = factor(df$gender, levels=c(1,2), labels =c("male","female"))
-# df$is_churn = factor(df$is_churn, levels= c(0,1), labels=c("not churn","churn"))
-# df$is_auto_renew = factor(df$is_auto_renew, levels= c(0,1), labels=c("non-auto renewal","auto-renewal"))
-# df$is_cancel = factor(df$is_cancel, levels= c(0,1), labels=c("non-cancellation","cancellation"))
-# df$city = factor(df$city)
-# df$registered_via = factor(df$registered_via)
-# df$payment_plan_days = factor(df$payment_plan_days)
-# df$payment_method_id = factor(df$payment_method_id)
-# df = na.omit(df)
-# summary(df)
-
-#### Data Cleaning ####
+#### END Data Cleaning ####
 
 #train-test split
 library(caTools)
@@ -130,6 +66,9 @@ set.seed(2407)
 
 colnames(finaldf)[1] = "is_churn"
 
+# dashtrain <- sample.split(Y = cleandf$is_churn, SplitRatio = 0.7)
+# dashtest = subset(cleandf, train == F)
+
 train <- sample.split(Y = finaldf$is_churn, SplitRatio = 0.7)
 trainset.naive <- subset(finaldf, train == T)
 trainset.rose = ROSE(is_churn~., data=trainset.naive, seed=2407)$data
@@ -138,6 +77,9 @@ summary(trainset.naive$is_churn)
 summary(trainset.rose$is_churn)
 summary(testset$is_churn)
 
+write.csv(trainset.naive, "trainset_naive.csv")
+write.csv(trainset.rose, "trainset_rose.csv")
+write.csv(testset, "testset.csv")
 
 #### BEGIN LOGISTIC REGRESSION ####
 log.naive = glm(is_churn ~ ., family = binomial, data=trainset.naive)
@@ -153,15 +95,21 @@ caret::confusionMatrix(factor(log.class.naive), reference=factor(testset$is_chur
 auc(testset$is_churn, log.predict.naive)
 pr.curve(log.predict.naive[testset$is_churn == 1], log.predict.naive[testset$is_churn == 0])
 
-#get coefficients
-exp(coef(log.naive))
-
 # Confusion Matrix ROSE
 log.predict.rose <- predict(log.rose, newdata = testset, type = 'response')
 log.class.rose <- ifelse(log.predict.rose > threshold, 1, 0)
 caret::confusionMatrix(as.factor(log.class.rose), reference=factor(testset$is_churn), positive="1", mode="prec_recall")
 auc(testset$is_churn, log.predict.rose)
 pr.curve(log.predict.rose[testset$is_churn == 1], log.predict.rose[testset$is_churn == 0])
+
+#get importance
+coeffs = data.frame(varImp(log.rose))
+rownames(coeffs)[rownames(coeffs) == "bd"] = "bd1"
+rownames(coeffs)[rownames(coeffs) == "actual_amount_paid"] = "actual_amount_paid1"
+rownames(coeffs) = lapply(rownames(coeffs), function(x) substr(x,1,nchar(x)-1))
+
+# dashboard = cbind(dashtest, PR=log.predict.rose)
+# write.csv(dashboard, "dashboard.csv", row.names = F)
 
 #### END LOGISTIC REGRESSION ####
 
@@ -179,7 +127,6 @@ while (cart$cptable[i,j] > CVerror.cap) {
 }
 cart.cp = ifelse(i > 1, sqrt(cart$cptable[i,1] * cart$cptable[i-1,1]), 1)
 cart.naive = prune(cart, cp = cart.cp)
-cart.naive$variable.importance
 
 #proportion of importance
 #round(100*cart.naive$variable.importance/sum(cart.naive$variable.importance))
@@ -196,7 +143,7 @@ while (cart$cptable[i,j] > CVerror.cap) {
 }
 cart.cp = ifelse(i > 1, sqrt(cart$cptable[i,1] * cart$cptable[i-1,1]), 1)
 cart.rose = prune(cart, cp = cart.cp)
-cart.rose$variable.importance
+# cart.rose$variable.importance
 round(100*cart.rose$variable.importance/sum(cart.rose$variable.importance))
 
 # confusion matrix naive
@@ -214,45 +161,6 @@ auc(testset$is_churn, cart.predict.rose)
 pr.curve(cart.predict.rose[testset$is_churn == "1"], cart.predict.rose[testset$is_churn == "0"])
 #### END OF CART ####
 
-#### BEGIN NEURAL NET ####
-library(nnet)
-library(neuralnet)
-set.seed(2407)
-temp = trainset
-temp$is_churn = ifelse(temp$is_churn == "churn", 1, 0)
-temp$is_auto_renew = ifelse(temp$is_auto_renew == "auto-renewal", 1, 0)
-temp$is_cancel = ifelse(temp$is_cancel == "non-cancellation", 0, 1)
-temp$payment_method_id = as.numeric(temp$payment_method_id)
-temp$payment_plan_days = as.numeric(temp$payment_plan_days)
-
-temp_test = testset
-temp_test$is_churn = ifelse(temp_test$is_churn == "churn", 1, 0)
-temp_test$is_auto_renew = ifelse(temp_test$is_auto_renew == "auto-renewal", 1, 0)
-temp_test$is_cancel = ifelse(temp_test$is_cancel == "non-cancellation", 0, 1)
-temp_test$payment_method_id = as.numeric(temp_test$payment_method_id)
-temp_test$payment_plan_days = as.numeric(temp_test$payment_plan_days)
-
-
-nnmodel = neuralnet(is_churn ~ is_auto_renew + actual_amount_paid + payment_method_id +
-                      plan_list_price + payment_plan_days + is_cancel, data = temp,hidden=2, err.fct="ce", linear.output=FALSE )
-
-nn.predict <- ifelse(unlist(nnmodel$net.result) > 0.5, 1, 0)
-cat('Trainset Confusion Matrix with neuralnet (1 hidden layer, 2 hidden nodes, Unscaled X):')
-table(temp$is_churn, nn.predict)
-# Overall Accuracy
-mean(nn.predict == temp$is_churn)
-
-#Test the resulting output
-nn.results <- compute(nnmodel, temp_test)
-results <- data.frame(actual = temp_test$is_churn, prediction = nn.results$net.result)
-roundedresults<-sapply(results,round,digits=0)
-roundedresultsdf=data.frame(roundedresults)
-attach(roundedresultsdf)
-table(actual,prediction)
-roundedresultsdf
-mean(roundedresultsdf$actual == roundedresultsdf$prediction)
-#### END NEURAL NETWORK ####
-
 #### BEGIN random forest ####
 library(randomForest)
 set.seed(2407)
@@ -262,8 +170,8 @@ rf.naive = randomForest(is_churn ~ ., data = trainset.naive, importance=T,
 rf.predict.naive = predict(rf.naive, newdata = testset, type="prob")
 rf.class.naive = ifelse(rf.predict.naive[,"1"] > threshold, 1, 0)
 caret::confusionMatrix(as.factor(rf.class.naive), reference=testset$is_churn, positive="1", mode="prec_recall")
-pr.curve(rf.predict.naive[testset$is_churn == "1", "1"], rf.predict.naive[testset$is_churn == "0", "1"])
 auc(testset$is_churn, rf.predict.naive[,"1"])
+pr.curve(rf.predict.naive[testset$is_churn == "1", "1"], rf.predict.naive[testset$is_churn == "0", "1"])
 
 rf.rose = randomForest(is_churn ~ ., data = trainset.rose, importance=T,
                        ntree=100)
@@ -271,8 +179,9 @@ rf.predict.rose = predict(rf.rose, newdata = testset, type="prob")[,"1"]
 
 rf.class.rose = ifelse(rf.predict.rose > threshold, 1, 0)
 caret::confusionMatrix(as.factor(rf.class.rose), reference=testset$is_churn, positive="1", mode="everything")
-pr.curve(rf.predict.rose[testset$is_churn == "1"], rf.predict.rose[testset$is_churn == "0"], curve=T)
 auc(testset$is_churn, rf.predict.rose)
+pr.curve(rf.predict.rose[testset$is_churn == "1"], rf.predict.rose[testset$is_churn == "0"], curve=T)
+
 plot(roc(testset$is_churn, rf.predict.rose, plot=T))
 plot(rf.pr.rose)
 
